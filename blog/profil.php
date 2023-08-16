45ef4a7c7d2ce5636585932fde1b6062a6d5b1a4
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2023-08-16 15:04:45 +0300

    profile page updated

--- blog/profil.php
+++ blog/profil.php
@@ -2,6 +2,11 @@
     include("header.php");
 ?>
 
-<div class="container">
-    Ben Kimim?
+<div class="container profil">
+    <h3 style="text-align: center;">
+        BEN KİMİM?
+    </h3>
+    <p style="text-align: center; padding-bottom: 20px;">
+        Merhaba! Benim adım Abdulkadir Çağlar. Bu blog sitesini bir düşünce paylaşma platformu olarak oluşturdum. Umarım beğenirsiniz.
+    </p>
 </div>
